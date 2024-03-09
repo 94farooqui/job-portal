f6b052cb22aad69c7f4b19875a36f147a4878f90
94farooqui <94.farooqui@gmail.com>
2024-03-09 10:55:29 +0400

    added client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-blue-400">Hello World</h2>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  )
+}
+
+export default App;
